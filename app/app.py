4627a88e504767afc0b8880e94ee698ba877c038
Paulo Cavalcante <paulocassuncao@gmail.com>
2016-01-29 19:18:57 +0000

    Modify app.run

--- app/app.py
+++ app/app.py
@@ -70,4 +70,4 @@ def not_found(error):
     return make_response(jsonify({'error' : 'Not found!'}), 404)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run()
